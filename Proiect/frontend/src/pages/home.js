0bd80ceb1cef37b2b55ab5b9fa57fd4c026e421d
andreicalinciuc <scoala.andrei@yahoo.com>
2020-01-24 20:57:07 +0200

    action add event

--- Proiect/frontend/src/pages/home.js
+++ Proiect/frontend/src/pages/home.js
@@ -2,23 +2,48 @@ let Home = {
     render: async() => {
         return `
         <header class="header">
-        <img src="src/assets/images/logo.png" width="100px" height="100px">
+        <img src="./assets/images/logo.png" width="100px" height="100px">
         <p class="text"> Pagini</p>
-    </header> 
-   
-    <div class="line"></div>
-    <p class="add-party">Creeaza o noua petrecere/ Intra la petrecere</p>
+    </header>
 
+    <div class="line"></div>
+    <div class="wrap">
+        <button type="button" class="open-button" onclick='openForm()'>Creeaza o noua petrecere/ Intra la
+            petrecere</button>
+    </div>
     <div class="block-party">
         <div class="party">1</div>
         <div class="party">2</div>
         <div class="party">3</div>
         <div class="party">4</div>
     </div>
+
+    <div class="form-popup" id="myForm">
+        <form action="/action_page.php" class="form-container">
+            <h1>Login</h1>
+
+            <label for="email"><b>Email</b></label>
+            <input type="text" placeholder="Enter Email" name="email" required>
+
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+
+            <button type="submit" class="btn">Login</button>
+            <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+        </form>
+    </div>
             
         `
     },
-    after_render: async () => {}
+    after_render: async () => {
+
+        function openForm() {
+            document.getElementById("myForm").style.display = "block";
+        }
+        function closeForm() {
+            document.getElementById("myForm").style.display = "none";
+        }
+    }
 }
 
 export default Home
