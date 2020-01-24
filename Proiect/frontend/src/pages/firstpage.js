247b4a887f8c20985e6e203ba92c43d6bd0d3e1e
andratampu <tampu.andra@yahoo.ro>
2020-01-24 20:21:11 +0200

    home page done

--- Proiect/frontend/src/pages/firstpage.js
+++ Proiect/frontend/src/pages/firstpage.js
@@ -1,34 +1,31 @@
-const onSubmit =  async (values) => {
+const onSubmit = async (values) => {
     console.log(values)
 }
 let FirstPage = {
-    render: async() => {
+    render: async () => {
         return `
-            <div class="page-wrapper">
-                <div class="login-page">
-                    <div class="login-logo">
-                        <h1 class="welcome_firstpage">Bine ai venit!</h1>
-                    </div>
+        <div class="page-wrapper">
+            <div class="home-page">
+                <div class="login-logo">
+                    <h1 class="welcome">Bine ai venit!</h1>
+                </div>
+                <div class="container">
                     <div class="logo">
-                        <img src="./images/" alt="Smiley face" height="42" width="42">
+                    </div>
+                    <div class="block-user-actions">
+                        <div class="button-div">
+                            <a href="/login">Autentificare</a>
+                        </div>
+                        <div class="button-div">
+                            <a href="/signup">Inregistrare</a>
+                        </div>
                     </div>
                 </div>
             </div>
-            
+        </div>
         `
     },
     after_render: async () => {
-        document.getElementById('submit-button').addEventListener('click', async () => {
-            console.log('aici')
-            let formValues={}
-            let email = document.getElementById('email_login')
-            let password = document.getElementById('password_login')
-            formValues = {
-                email:email.value,
-                password:password.value
-            }
-            await onSubmit(formValues)
-        })
     }
 }
 
