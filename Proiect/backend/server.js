5ce767d93c1c92b80c16a249887157d2aceb99e2
robertzanceanu <43026706+robertzanceanu@users.noreply.github.com>
2020-01-25 20:01:11 +0200

    Parties routes

--- Proiect/backend/server.js
+++ Proiect/backend/server.js
@@ -4,6 +4,8 @@ const app = express()
 const dotenv = require('dotenv')
 const authRoute = require('./routes/auth')
 const postRoute = require('./routes/posts')
+const partiesRoute = require('./routes/parties')
+
 var cors = require('cors')
 
 dotenv.config()
@@ -17,6 +19,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/user', authRoute)
 app.use('/api/posts',postRoute)
-
+app.use('/api/parties',partiesRoute)
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
