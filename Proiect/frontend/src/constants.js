97fd54a66dc29e276baafcd54a1be88adae46fb0
andratampu <tampu.andra@yahoo.ro>
2020-01-30 09:21:42 +0200

    Arhitectura actualizata

--- Proiect/frontend/src/constants.js
+++ Proiect/frontend/src/constants.js
@@ -1,2 +1,2 @@
-// export const API_URL = "http://192.168.43.156:3000/api"
-export const API_URL = "http://192.168.0.173:3000/api"
+export const API_URL = "http://192.168.43.156:3000/api"
+//export const API_URL = "http://192.168.0.173:3000/api"
